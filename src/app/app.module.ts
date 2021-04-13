231e208f2031f21c98937a3c838ad9beebf72ef8
Steve Witman <stevewitman@gmail.com>
2021-04-13 09:48:00 -0600

    scaffold basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VertTabsComponent } from './vert-tabs/vert-tabs.component';
+import { ExampleOneComponent } from './example-one/example-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VertTabsComponent,
+    ExampleOneComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/vert-tabs/vert-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vert-tabs',
+  templateUrl: './vert-tabs.component.html',
+  styleUrls: ['./vert-tabs.component.scss']
+})
+export class VertTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
